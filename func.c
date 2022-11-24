1d1f754a0641901761e23715d32fb87dbd92de5e
Wang Lu <cnwanglu@icloud.com>
2022-11-24 09:56:38 -0800

    c99: extern inline

--- func.c
+++ func.c
@@ -1,7 +1,7 @@
 #include "func.h"
 #include <stdio.h>
 
-inline int add_inline(int a, int b)
+extern inline int add_inline(int a, int b)
 {
     return a + b;
 }
@@ -9,7 +9,6 @@ inline int add_inline(int a, int b)
 int extern_func(int a, int b)
 {
     // if the above inline function is optimized failed,
-    // it will be regared as externel declaration(definition in main.c)
+    // it will be regared as normal function(cause multiple definition)
     return add_inline(a,b);
-    /* return 0; */
 }
