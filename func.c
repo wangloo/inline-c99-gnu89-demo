b4891606834a5852965648e1abc9a56b47bed33f
Wang Lu <cnwanglu@icloud.com>
2022-11-24 09:31:16 -0800

    gnu89: inline

--- func.c
+++ func.c
@@ -1,6 +1,6 @@
 #include "func.h"
 
-static inline int add_inline(int a, int b)
+inline int add_inline(int a, int b)
 {
     return a + b;
 }
@@ -8,6 +8,6 @@ static inline int add_inline(int a, int b)
 int extern_func(int a, int b)
 {
     // if the above inline function is optimized failed,
-    // it will be regared as normal static function(test in main.c)
+    // it will be regared as normal function(test in main.c)
     return add_inline(a,b);
 }
